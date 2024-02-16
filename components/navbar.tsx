21763a7fe2d11ac9895f257bb181d01299e5ac7a
Mrlion <mrlion4202@gmail.com>
2024-02-16 16:04:57 +0000

    Changed and deleted some things

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Nav() {
+  return (
+    <nav className="flex justify-between lg:justify-around items-center py-5 px-4 lg:px-0">
+      <h3>
+        Mrlion
+      </h3>
+    </nav>
+  );
+}
