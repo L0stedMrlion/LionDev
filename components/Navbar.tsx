641f2dda774b467aa42e0c096de2f496e2f19a05
Mrlion <mrlion4202@gmail.com>
2024-02-16 16:10:40 +0000

    some changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function Nav() {
+export function Navbar() {
   return (
     <nav className="flex justify-between lg:justify-around items-center py-5 px-4 lg:px-0">
       <h3>
